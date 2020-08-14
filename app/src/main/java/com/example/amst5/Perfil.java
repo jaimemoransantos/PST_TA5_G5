1d6d9f9aa2f3c0aedd1029f12124449566bba6f7
Alex Mora Hurel <Aleemora@espol.edu.ec>
2020-08-14 15:29:57 -0500

    Se le dio fomato a los botones de Perfiles ademas de asignarle la funcion a cada uno de ellos.

--- app/src/main/java/com/example/amst5/Perfil.java
+++ app/src/main/java/com/example/amst5/Perfil.java
@@ -2,7 +2,11 @@ package com.example.amst5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.InputStreamReader;
 
 public class Perfil extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class Perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+    }
+
+    public void categoria(View view){
+        Intent i = new Intent(this, CategoriaC.class);
+        //i.putExtra("direccion", et1.getText().toString());
+        startActivity(i);
+        finish();
+    }
+    public void perfil(View view){
+        Intent i = new Intent(this, Perfil.class);
+        //i.putExtra("direccion", et1.getText().toString());
+        startActivity(i);
+        finish();
+    }
+    public void principal(View view){
+        Intent i = new Intent(this, Principal.class);
+        //i.putExtra("direccion", et1.getText().toString());
+        startActivity(i);
+        finish();
+    }
+
+    public void  salir(View view){
+        finish();
     }
 }
