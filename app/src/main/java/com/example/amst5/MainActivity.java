567d46fb7cece811ed66f6607207a0743479595f
Jaime Moran <jaimemoransantos@gmail.com>
2020-08-13 20:33:30 -0500

    Prueba Jaime

--- app/src/main/java/com/example/amst5/MainActivity.java
+++ app/src/main/java/com/example/amst5/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // Prueba #1 para ver que ocurre
         //Prueba2
+        //Prueba Jaime
     }
 }
