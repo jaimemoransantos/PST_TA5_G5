a5328004b682b226cc63cfb3a7897949d043a040
Alex Mora Hurel <Aleemora@espol.edu.ec>
2020-08-13 20:27:50 -0500

    Prueba a mi rama 2

--- app/src/main/java/com/example/amst5/MainActivity.java
+++ app/src/main/java/com/example/amst5/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // Prueba #1 para ver que ocurre
-        //Prueba2 
+        //Prueba2
     }
 }
