f581700f31d5f9fcb200a09a77c62a98c611ec42
Alex Mora Hurel <Aleemora@espol.edu.ec>
2020-08-13 23:56:51 -0500

    Se añadio la actividad de registro y se crearon todos os elementos en el archivo .xml, en donde se puede completar el registro.

--- app/src/main/java/com/example/amst5/MainActivity.java
+++ app/src/main/java/com/example/amst5/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.amst5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void registro(View view){
+        Intent i = new Intent(this, Registro.class );
+        startActivity(i);
+    }
+    public void ingresar(View view){
+        Intent prin = new Intent(MainActivity.this, Principal.class );
+        startActivity(prin);
     }
 }
